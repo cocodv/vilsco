ad561d12b2c2d734aa3d57e5e790c593da1c7bbe
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2026-02-09 15:26:24 +0100

    sasa

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // include JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
