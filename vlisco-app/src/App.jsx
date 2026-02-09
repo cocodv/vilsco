d9e5a9e34a771810ea7b7253b5d8eabda10fbb7b
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2026-02-09 15:15:00 +0100

    sad

---
+++ vlisco-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import FounderSection from './components/FounderSection';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <FounderSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
